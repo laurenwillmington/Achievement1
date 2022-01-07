21dd286ef283396d0e124215ff8aaa41e71b8759
laurenwillmington <96395547+laurenwillmington@users.noreply.github.com>
2022-01-07 11:31:52 -0600

    Added CSS styling to pokemon app
    added changes to border, change, & font. Created a loop and conditional.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 
-let piccachu = {
-    name: 'piccachu',
-    height: 2,
-    types: ['water','lightning','fire']
+let pokemonList =[
+    {
+        name: 'piccachu',
+        height: 2,
+        types: ['water','lightning','fire']
+    },
+
+    {
+        name: 'bulbasar',
+        height: 5,
+        types: ['wind', 'strength']
+    },
+
+    {
+        name: 'evy',
+        height: 1,
+        types: ['spinkick', 'cuteness']
+    }
+]
+
+for (i=0; i < pokemonList.length; i++) {
+    document.write(
+        "<p class='pokemon'>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ")" + (pokemonList[i].height > 3 ? ' - Wow, That is big' : '') +  "</p>"
+    ); 
 }
 
-let bulbasar = {
-    name: 'bulbasar',
-    height: 5,
-    types: ['wind', 'strength']
-}
-let pokemonList =[piccachu,bulbasar]
