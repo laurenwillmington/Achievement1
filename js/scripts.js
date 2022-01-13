146d8ae040ed8f6fbaded3e6c3eef032b0d78f5a
laurenwillmington <96395547+laurenwillmington@users.noreply.github.com>
2022-01-13 10:59:17 -0600

    Updated Pokemon List to IFFE
    Changed the for loop to a forEach and put the pokemonList in an IFFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,47 @@
 
-let pokemonList =[
-    {
-        name: 'piccachu',
-        height: 2,
-        types: ['water','lightning','fire']
-    },
 
-    {
-        name: 'bulbasar',
-        height: 5,
-        types: ['wind', 'strength']
-    },
 
-    {
-        name: 'evy',
-        height: 1,
-        types: ['spinkick', 'cuteness']
-    }
-]
+    const pokemonRepository = (function(){
+        let pokemonList = [
+            {
+                name: 'piccachu',
+                height: 2,
+                types: ['water','lightning','fire']
+            },
+        
+            {
+                name: 'bulbasar',
+                height: 5,
+                types: ['wind', 'strength']
+            },
+        
+            {
+                name: 'evy',
+                height: 1,
+                types: ['spinkick', 'cuteness']
+            }
+        ]
+        return {
+            add: function(addNewPokemon){
+                pokemonList.push(addNewPokemon)
+            },
+            getAll: function(){
+                return pokemonList
+            }
+        }
+    })()
 
-for (i=0; i < pokemonList.length; i++) {
-    document.write(
-        "<p class='pokemon'>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ")" + (pokemonList[i].height > 3 ? ' - Wow, That is big' : '') +  "</p>"
+
+
+
+// for (i=0; i < pokemonList.length; i++) {
+//     document.write(
+//         "<p class='pokemon'>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ")" + (pokemonList[i].height > 3 ? ' - Wow, That is big' : '') +  "</p>"
+//     ); 
+// }
+
+pokemonRepository.getAll().forEach(function(pokemon){
+        document.write(
+        "<p class='pokemon'>" + pokemon.name + ' (height: ' + pokemon.height + ")" + (pokemon.height > 3 ? ' - Wow, That is big' : '') +  "</p>"
     ); 
-}
-
+})
